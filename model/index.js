1b3a2a9e555c745e32362ded69427020bac65f9d
Tiffany Marko <tiffanyjmarko@gmail.com>
2022-11-08 13:07:30 -0800

    user model

---
+++ model/index.js
@@ -0,0 +1,47 @@
+// const Friends = require('./friends');
+// const Games = require('./games');
+// // const Profile = require('./profile');
+// const User = require('./user');
+// const Collection = require ('');
+// const Comments = re
+
+// Friends.belongsToMany(, {
+//   //table needed to store the foreign keys?
+//   through: {
+//     model: ,
+//     unique: false
+//   },
+//   // alias for when data is retrieved?
+//   as: ''
+// });
+
+// Games.belongsToMany(, {
+//   //table needed to store the foreign keys?
+//   through: {
+//     model: ,
+//     unique: false
+//   },
+//   // alias for when data is retrieved?
+//   as: ''
+// });
+
+// Profile.belongsToMany(, {
+//     //table needed to store the foreign keys?
+//     through: {
+//       model: ,
+//       unique: false
+//     },
+//     // alias for when data is retrieved?
+//     as: ''
+//   });
+
+//   User.belongsToMany(, {
+//     //table needed to store the foreign keys?
+//     through: {
+//       model: ,
+//       unique: false
+//     },
+//     // alias for when data is retrieved?
+//     as: ''
+//   });
+// module.exports = { Friends, Games, Profile, User };
