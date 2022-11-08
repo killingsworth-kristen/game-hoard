cd878d5057e116e04eb32a0bc5b8f915d8ece347
Darryl Tillman <113705249+Tales64@users.noreply.github.com>
2022-11-08 14:06:08 -0800

    updates

---
+++ seeds/seed.js
@@ -0,0 +1,16 @@
+INSERT INTO company (game,players,) 
+VALUES ("Monopoly","8",""),
+        ("Risk","6"),
+        ("Sorry",,""),
+        ("Stratigo",""),
+        ("Munchkin",""),
+        ("One Night",""),
+        ("Dragoon","");
+
+
+INSERT INTO characters (superidentity, altEgo, nemisis,comic_company) 
+VALUES ("Superman","Clark Kent","Lex Luthor"),
+        ("Spider-Man","Peter Parker","Green Goblin"),
+        ("Spawn","Albert Simmons","Violator"),
+        ("Raphael","Raff","Shreder"),
+        ("Savage Dragon","Kurr","The Overlord");
